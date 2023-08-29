7fbced3c49fdc821b2c57b5045abdde3c846206b
ShawnGao <2519306859@qq.com>
2023-08-29 17:09:10 +0800

    :ambulance: Modify SPI1 initialization to RFID-RC522 SPI1 initialization.

--- Src/main.c
+++ Src/main.c
@@ -264,7 +264,7 @@ static void MX_CRC_Init(void) {
 static void MX_SPI1_Init(void) {
 
     /* USER CODE BEGIN SPI1_Init 0 */
-
+    __HAL_RCC_SPI1_CLK_ENABLE();
     /* USER CODE END SPI1_Init 0 */
 
     /* USER CODE BEGIN SPI1_Init 1 */
@@ -275,19 +275,19 @@ static void MX_SPI1_Init(void) {
     hspi1.Init.Mode = SPI_MODE_MASTER;
     hspi1.Init.Direction = SPI_DIRECTION_2LINES;
     hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
+    hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
     hspi1.Init.NSS = SPI_NSS_SOFT;
-    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
     hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
     hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
     hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-    hspi1.Init.CRCPolynomial = 10;
+    hspi1.Init.CRCPolynomial = 7;
     if (HAL_SPI_Init(&hspi1) != HAL_OK) {
         Error_Handler();
     }
     /* USER CODE BEGIN SPI1_Init 2 */
-
+    __HAL_SPI_ENABLE(&hspi1);
     /* USER CODE END SPI1_Init 2 */
 
 }
