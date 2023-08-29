4d5c3736ab9c8de146cd87966652a12876aa77d9
ShawnGao <2519306859@qq.com>
2023-08-29 17:49:17 +0800

    :fire: Remove duplicate code.

--- Drivers/IO/Src/rfid.c
+++ Drivers/IO/Src/rfid.c
@@ -75,23 +75,6 @@ void RFID_INIT_GPIO() {
     gpioInitTypeDef.Mode = GPIO_MODE_OUTPUT_PP;
     gpioInitTypeDef.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(RFID_RST_PIN_G, &gpioInitTypeDef);
-
-    hspi1.Instance = SPI1;
-    hspi1.Init.Mode = SPI_MODE_MASTER;
-    hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-    hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
-    hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
-    hspi1.Init.NSS = SPI_NSS_SOFT;
-    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
-    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-    hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-    hspi1.Init.CRCPolynomial = 7;
-    if (HAL_SPI_Init(&hspi1) != HAL_OK) {
-        Error_Handler();
-    }
-    __HAL_SPI_ENABLE(&hspi1);
 }
 
 void RFID_INIT() {
