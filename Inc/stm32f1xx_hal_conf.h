47babe05ef15a8e3e952736351e82d5cd6ed5339
ShawnGao <2519306859@qq.com>
2023-09-26 19:33:50 +0800

    :fire: Disable SPI CRC.

--- Inc/stm32f1xx_hal_conf.h
+++ Inc/stm32f1xx_hal_conf.h
@@ -227,7 +227,7 @@
 * Deactivated: CRC code cleaned from driver
 */
 
-#define USE_SPI_CRC                     1U
+#define USE_SPI_CRC                     0U
 
 /* Includes ------------------------------------------------------------------*/
 /**
