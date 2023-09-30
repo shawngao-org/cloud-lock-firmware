0876f93858acb376c9dd3cd885a8f14c715f2b4f
ShawnGao <2519306859@qq.com>
2023-09-30 16:01:15 +0800

    :sparkles: Handled the initialization of WiFi and time of ESP8266.(Configure the config.ini file from the SD card.)

--- Drivers/IO/Inc/esp8266.h
+++ Drivers/IO/Inc/esp8266.h
@@ -12,36 +12,52 @@
 #include "stdio.h"
 #include "string.h"
 #include "main.h"
+#include "ini.h"
+#include "math.h"
+#include "time.h"
 
-#define WIFI_SSID "OpenWrt_2.4G"
-#define WIFI_PWD "network1925"
-
-#define SERVER_HOST "mqtt.sgtu.ltd"
-#define SERVER_PORT "1883"
+/**
+ * [mqtt]
+ * host=
+ * port=
+ * client_id=
+ * user=
+ * password=
+ * subscription=
+ * [wifi]
+ * ssid=
+ * password=
+ */
 
 #define TEST "AT\r\n"
 #define RST "AT+RST\r\n"
 #define RESTORE "AT+RESTORE\r\n"
 
-#define SET_WIFI_MODE "AT+CWMODE=1\r\n"
-#define ENABLE_SMART_CONFIG "AT+CWSTARTSMART\r\n"
-#define DISABLE_SMART_CONFIG "AT+CWSTOPSMART\r\n"
+#define SET_WIFI_MODE_1 "AT+CWMODE=1\r\n"
+#define GET_WIFI_STATUS "AT+CWJAP?\r\n"
+#define CONN_WIFI "AT+CWJAP=\"%s\",\"%s\"\r\n"
+
+// #define TEST_WIFI "AT+CWRECONNCFG?\r\n"
 
 #define SET_SNTP "AT+CIPSNTPCFG=1,8,\"cn.ntp.org.cn\",\"ntp.sjtu.edu.cn\"\r\n"
+#define GET_TIME "AT+CIPSNTPTIME?\r\n"
+#define GET_TIMESTAMP "AT+SYSTIMESTAMP?\r\n"
 
 #define SET_USER_INFO "AT+MQTTUSERCFG=0,%d,\"%s\",\"%s\",\"%s\",0,0,\"%s\"\r\n"
 #define CONN_MQTT "AT+MQTTCONN=0,\"%s\",%d,0\r\n"
 #define MQTT_SUB "AT+MQTTSUB=0,\"%s\",1\r\n"
 #define MQTT_PUB "AT+MQTTPUB=0,\"%s\",\"%s\",1,0\r\n"
 
-#define REV_OK 0
-#define REV_WAIT 1
-
 void ESP8266_CLR();
+
 _Bool ESP8266_WAIT_REV();
-_Bool ESP8266_SEND_CMD(char *cmd, char *res);
-void ESP8266_SEND_DATA(unsigned char *dat, unsigned short len);
-unsigned char *ESP8266_GET_IPD(unsigned short timeout);
+
+_Bool ESP8266_SEND_CMD(char *, char *);
+
+void ESP8266_SEND_DATA(unsigned char *, unsigned short);
+
+unsigned char *ESP8266_GET_IPD(unsigned short);
+
 void ESP8266_INIT();
 
 #endif //CLOUD_LOCK_ESP8266_H
--- Drivers/IO/Src/esp8266.c
+++ Drivers/IO/Src/esp8266.c
@@ -15,6 +15,10 @@ void ESP8266_CLR() {
     esp8266_cnt = 0;
 }
 
+int ESP_RD_INI_CONF(char *title, char *key, char *buf) {
+    return getIniKey(CONF_FILE, title, key, buf);
+}
+
 int ESP8266_EXEC(char *cmd, char *result) {
     ESP8266_CLR();
     USART2_Tx_String(cmd);
@@ -26,6 +30,87 @@ int ESP8266_EXEC(char *cmd, char *result) {
     return 1;
 }
 
+void SET_TIME() {
+    USART1_Tx_String("Set NTP ...");
+    while (ESP8266_EXEC(SET_SNTP, "OK")) {
+        delay_ms(500);
+        USART1_Tx_String(".");
+    }
+    USART1_Tx_String("OK\n");
+    USART1_Tx_String("Get Time ...");
+    while (ESP8266_EXEC(GET_TIME, "OK") && strstr((char *) esp8266_buf, "1970") == NULL) {
+        delay_ms(500);
+        USART1_Tx_String(".");
+    }
+    USART1_Tx_String("\n");
+    while (ESP8266_EXEC(GET_TIMESTAMP, "OK")) {
+        delay_ms(500);
+        USART1_Tx_String(".");
+    }
+    USART1_Tx_String((char *) esp8266_buf);
+    unsigned char timestamp_str[11];
+    for (int i = 0, j = 0; esp8266_buf[i] != '\0'; ++i) {
+        if (esp8266_buf[i] >= '0' && esp8266_buf[i] <= '9' && j < 10) {
+            timestamp_str[j++] = esp8266_buf[i];
+        }
+    }
+    timestamp_str[10] = '\0';
+    time_t timestamp = 0;
+    int len = 9;
+    for (int i = len; i >= 0; --i) {
+        if (timestamp_str[i] >= '0' && timestamp_str[i] <= '9') {
+            timestamp += pow(10 * 1.0, (len - i) * 1.0) * (timestamp_str[i] - '0');
+        }
+    }
+    timestamp += 8 * 60 * 60;
+    struct tm tm_t = *localtime(&timestamp);
+    RTC_DateTypeDef rtcDateTypeDef;
+    RTC_TimeTypeDef rtcTimeTypeDef;
+    rtcDateTypeDef.Year = tm_t.tm_year - 100;
+    rtcDateTypeDef.Month = tm_t.tm_mon + 1;
+    rtcDateTypeDef.Date = tm_t.tm_mday;
+    rtcTimeTypeDef.Hours = tm_t.tm_hour;
+    rtcTimeTypeDef.Minutes = tm_t.tm_min;
+    rtcTimeTypeDef.Seconds = tm_t.tm_sec + 5;
+    HAL_RTC_SetTime(&hrtc, &rtcTimeTypeDef, RTC_FORMAT_BIN);
+    HAL_RTC_SetDate(&hrtc, &rtcDateTypeDef, RTC_FORMAT_BIN);
+    USART1_Tx_String("\n");
+    USART1_Tx_String("OK\n");
+}
+
+void SET_WIFI() {
+    USART1_Tx_String("Set WiFi Mode 1 ...");
+    while (ESP8266_EXEC(SET_WIFI_MODE_1, "OK")) {
+        delay_ms(500);
+        USART1_Tx_String(".");
+    }
+    USART1_Tx_String("OK\n");
+    USART1_Tx_String("Get WiFi Status ...");
+    while (ESP8266_EXEC(GET_WIFI_STATUS, "OK")) {
+        delay_ms(500);
+        USART1_Tx_String(".");
+    }
+    USART1_Tx_String("OK\n");
+    if (strstr((char *) esp8266_buf, "+CWJAP")) {
+        USART1_Tx_String("WiFi config ...");
+        char conn_wifi_str[128];
+        char ssid[32];
+        if (ESP_RD_INI_CONF("wifi", "ssid", ssid)) {
+            Error_Handler();
+        }
+        char passwd[32];
+        if (ESP_RD_INI_CONF("wifi", "password", passwd)) {
+            Error_Handler();
+        }
+        sprintf(conn_wifi_str, CONN_WIFI, ssid, passwd);
+        while (ESP8266_EXEC(conn_wifi_str, "GOT IP")) {
+            USART1_Tx_String(".");
+            delay_ms(500);
+        }
+        USART1_Tx_String("OK\n");
+    }
+}
+
 void ESP8266_INIT() {
     HAL_UART_Receive_IT(&huart2, &esp8266_rx_byte, 1);
     __HAL_UART_ENABLE_IT(&huart2, UART_IT_IDLE);
@@ -35,7 +120,7 @@ void ESP8266_INIT() {
         USART1_Tx_String(".");
     }
     USART1_Tx_String("OK\n");
-//    SET_WIFI();
+    SET_WIFI();
     SET_TIME();
 }
 
